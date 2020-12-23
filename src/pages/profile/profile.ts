dc9b0ecbc7d8dd592acd1319d132cb3e85123460
leojavadev <leojavadev@gmail.com>
2020-12-23 16:45:59 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -27,6 +27,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -65,6 +69,19 @@ export class ProfilePage {
       this.cameraOn = false;
     },
     (err) => {});
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {});
+  }
+
+  cancel(){
+    this.picture = null;
   }
 
 }
